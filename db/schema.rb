9a66bec0d1f3d52baffd72a1abc746461bd295c5
Pradyumna <pradyumna2905@gmail.com>
2015-09-07 16:50:27 -0700

    Create model for Chef and Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907043109) do
+ActiveRecord::Schema.define(version: 20150907234637) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
