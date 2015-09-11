1cdc55b452849fc041b7f59ec8754f68522a8e34
Pradyumna <pradyumna2905@gmail.com>
2015-09-10 22:44:53 -0700

    add tests for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+  validates :name, presence: true, length: {minimum: 5, maximum: 24}
+  validates :summary, presence: true, length: {minimum: 10, maximum: 150}
+  validates :description, presence: true, length: {minimum: 20, maximum: 500}
 end
