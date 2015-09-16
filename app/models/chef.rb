8da9f5fe775579d0e8788dbc4ff18c54a575db75
Pradyumna <pradyumna2905@gmail.com>
2015-09-15 19:17:40 -0700

    add chef_id as foreign key

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,7 +1,7 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
   before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :chefname, presence: true, length: {minimum: 3, maximum: 48}
   validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
-
 end
