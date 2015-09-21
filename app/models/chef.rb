67a6833e681f5a9f27e02c0bc65effb0275ff2a7
Pradyumna <pradyumna2905@gmail.com>
2015-09-20 19:10:15 -0700

    Add authentication page Users can now sign up for the app using their email

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,6 @@ class Chef < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :chefname, presence: true, length: {minimum: 3, maximum: 48}
   validates :email, presence: true, length: {maximum: 105}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+
 end
