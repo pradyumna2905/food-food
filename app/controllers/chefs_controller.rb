8d6769f64f31bec931ce561ea1730e0f461b6dde
Pradyumna <pradyumna2905@gmail.com>
2015-09-22 21:46:03 -0700

    Fix log in issues, restrict some access to non members

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -12,6 +12,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new(chef_params)
     if @chef.save
       flash[:success] = "Welcome to the kitchen. Start posting"
+      session[:chef_id] = @chef.id
       redirect_to recipes_path
     else
       render 'new'
