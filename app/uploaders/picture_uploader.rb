60eb91dbb276e162630ac26d2155549f767e473d
Pradyumna Shembekar <pradyumna2905@gmail.com>
2015-11-11 01:44:32 +0000

    fix aws s3 error

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  if Rails.env.environment?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
